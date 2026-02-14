f75a9209eaa0ed8bada31b11e8601906e9bb47d5
Jessica Jesus <jessicafj7@gmail.com>
2026-02-14 13:46:17 +0100

    Add new xcode project

---
+++ WishList/WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by Jessica Jesus on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
