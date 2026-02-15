fbaf98a96447efda2b753404a421b98b7295e44b
Jessica Jesus <jessicafj7@gmail.com>
2026-02-15 01:07:14 +0100

    Update empty wish list

--- WishList/WishList/Views/EmptyWishListView.swift
+++ WishList/WishList/Views/EmptyWishListView.swift
@@ -5,4 +5,29 @@
 //  Created by Jessica Jesus on 15/02/2026.
 //
 
-import Foundation
+import SwiftUI
+
+struct EmptyWishListView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Image(systemName: "heart.slash")
+                .font(.system(size: 70))
+                .foregroundColor(.gray.opacity(0.5))
+            
+            Text("Your wishlist is empty")
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Text("Start exploring and add attractions to your wishlist")
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 40)
+            
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
