8458d847279954805b3f6ca9d60c903fd821b54f
Jessica Jesus <jessicafj7@gmail.com>
2026-02-17 01:17:09 +0100

    Create tests

---
+++ WishList/WishList/Mocks/MockAttraction.swift
@@ -0,0 +1,38 @@
+//
+//  MockAttraction.swift
+//  WishList
+//
+//  Created by Jessica Jesus on 17/02/2026.
+//
+
+import Foundation
+
+struct MockAttraction {
+    func makeVenueAttraction() -> Attraction {
+        Attraction(
+            id: "id1",
+            name: "Van Gogh Museum",
+            type: .venue,
+            description: "Art museum with masterpieces",
+            location: "Amsterdam",
+            imageURL: "https://example.com/id1.jpg",
+            rating: 4.8,
+            price: 10.0,
+            priceCurrency: .eur
+        )
+    }
+    
+    func makeExhibitionAttraction() -> Attraction {
+        Attraction(
+            id: "id2",
+            name: "Surinamese School: Painting from Paramaribo to Amsterdam",
+            type: .exhibition,
+            description: "Art museum with masterpieces",
+            imageURL: "https://example.com/id2.jpg",
+            price: 10.0,
+            priceCurrency: .eur,
+            exhibitionStartDate: "20-02-2026",
+            exhibitionEndDate: "02-03-2026"
+        )
+    }
+}
